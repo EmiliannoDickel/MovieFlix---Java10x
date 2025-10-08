a18d1a6f378f9281c6bd3c0cb0c0a386e035df7f
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-07 22:22:05 -0300

    refactor: Refatorado codigo, adicionado dto e mapper e tambem implementado responseentity para resposta das consultas via

--- src/main/java/com/dickel/movieflix/controller/CategoryController.java
+++ src/main/java/com/dickel/movieflix/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.dickel.movieflix.controller;
 
 import com.dickel.movieflix.entity.Category;
+import com.dickel.movieflix.mapper.CategoryMapper;
+import com.dickel.movieflix.request.CategoryRequest;
+import com.dickel.movieflix.response.CategoryResponse;
 import com.dickel.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,29 +23,32 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping()
-    public List<Category> getAllCateroies() {
-        return categoryService.findAll();
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.findAll().stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList());
     }
 
     //PathVariable extrai o valor da variável id da URL
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
-        Optional<Category> categorySearch = categoryService.findById(id);
-        if (categorySearch.isPresent()) {
-            return categorySearch.get();
-        }
-        return null;
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id) {
+        return categoryService.findById(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     //RequestBody converte o JSON do corpo da requisição em um objeto Java
     @PostMapping
-    public Category saveCategory (@RequestBody Category category) {
-        return  categoryService.saveCategory(category);
+    public ResponseEntity<CategoryResponse> saveCategory (@RequestBody CategoryRequest request) {
+        Category newCategory = CategoryMapper.toCategoryRequest(request);
+        Category savedCategoty = categoryService.saveCategory(newCategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategoty));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategoryById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id) {
         categoryService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
