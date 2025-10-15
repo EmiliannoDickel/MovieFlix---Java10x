2febf67b92f894175b71ccc0126669d230044700
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 23:00:21 -0300

    feat: Adiciona o MovieRepository para acesso a dados

--- src/main/java/com/dickel/movieflix/repository/MovieRepository.java
+++ src/main/java/com/dickel/movieflix/repository/MovieRepository.java
@@ -1,4 +1,9 @@
 package com.dickel.movieflix.repository;
 
-public interface MovieRepository {
+import com.dickel.movieflix.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
