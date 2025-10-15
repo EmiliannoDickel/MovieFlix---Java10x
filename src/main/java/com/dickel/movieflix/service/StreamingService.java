aa5d302a2858153562cd80eef04ea515fb833341
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 23:03:18 -0300

    feat: Adiciona StreamingService com regras de negócio.

--- src/main/java/com/dickel/movieflix/service/StreamingService.java
+++ src/main/java/com/dickel/movieflix/service/StreamingService.java
@@ -1,4 +1,33 @@
 package com.dickel.movieflix.service;
 
+import com.dickel.movieflix.entity.Category;
+import com.dickel.movieflix.entity.Streaming;
+import com.dickel.movieflix.repository.StreamingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class StreamingService {
+
+    private final StreamingRepository streamingRepository;
+
+    public List<Streaming> findAll() {
+        return streamingRepository.findAll();
+    }
+
+    public Optional<Streaming> findById(Long id) {
+        return streamingRepository.findById(id);
+    }
+
+    public Streaming save (Streaming streaming) {
+        return streamingRepository.save(streaming);
+    }
+
+    public void deleteById(Long id) {
+        streamingRepository.deleteById(id);
+    }
 }
