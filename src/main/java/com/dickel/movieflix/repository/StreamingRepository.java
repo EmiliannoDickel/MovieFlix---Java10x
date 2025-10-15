bf0c9d8138aa74a4bed377073c8a16683bd7434d
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 23:00:50 -0300

    feat: Adiciona o StreamingRepository para acesso a dados

--- src/main/java/com/dickel/movieflix/repository/StreamingRepository.java
+++ src/main/java/com/dickel/movieflix/repository/StreamingRepository.java
@@ -1,4 +1,9 @@
 package com.dickel.movieflix.repository;
 
-public interface StreamingRepository {
+import com.dickel.movieflix.entity.Streaming;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StreamingRepository extends JpaRepository<Streaming, Long> {
 }
