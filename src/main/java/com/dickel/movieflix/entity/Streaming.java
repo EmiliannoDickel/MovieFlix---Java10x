2b45192fd44f12668e9fac4211970d3935d66a39
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 22:55:28 -0300

    feat: Adicionado entidade streaming.

--- src/main/java/com/dickel/movieflix/entity/Streaming.java
+++ src/main/java/com/dickel/movieflix/entity/Streaming.java
@@ -1,4 +1,24 @@
 package com.dickel.movieflix.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name = "streaming")
 public class Streaming {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
 }
