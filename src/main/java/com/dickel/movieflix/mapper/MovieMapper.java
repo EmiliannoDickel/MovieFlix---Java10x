d2502bb61caa5be78460475c1ab2c1b9a4488592
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 22:57:56 -0300

    feat: Adiciona DTOs (Request/Response) e Mapper da entidade movie.

--- src/main/java/com/dickel/movieflix/mapper/MovieMapper.java
+++ src/main/java/com/dickel/movieflix/mapper/MovieMapper.java
@@ -1,4 +1,55 @@
 package com.dickel.movieflix.mapper;
 
+import com.dickel.movieflix.entity.Category;
+import com.dickel.movieflix.entity.Movie;
+import com.dickel.movieflix.entity.Streaming;
+import com.dickel.movieflix.request.MovieRequest;
+import com.dickel.movieflix.response.CategoryResponse;
+import com.dickel.movieflix.response.MovieResponse;
+import com.dickel.movieflix.response.StreamingResponse;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+
+
+@UtilityClass
 public class MovieMapper {
+
+    public static Movie toMovieRequest (MovieRequest movieRequest) {
+
+        List<Category> categories = movieRequest.categories().stream()
+                .map(categoryId -> Category.builder().id(categoryId).build())
+                .toList();
+
+        List<Streaming> streamings = movieRequest.streamings().stream()
+                .map(streamingID -> Streaming.builder().id(streamingID).build())
+                .toList();
+
+        return Movie.builder()
+                .title(movieRequest.title())
+                .description(movieRequest.description())
+                .releaseDate(movieRequest.releaseDate())
+                .rating(movieRequest.rating())
+                .categories(categories)
+                .streamings(streamings)
+                .build();
+    }
+
+
+    public static MovieResponse toMovieResponse (Movie movie){
+
+       List<CategoryResponse> categories = movie.getCategories().stream().map(category -> CategoryMapper.toCategoryResponse(category))
+                .toList();
+        List<StreamingResponse> streamings = movie.getStreamings().stream().map(StreamingMapper::toStreamingResponse)
+                .toList();
+        return MovieResponse.builder()
+                .id(movie.getId())
+                .title(movie.getTitle())
+                .description(movie.getDescription())
+                .releaseDate(movie.getReleaseDate())
+                .rating(movie.getRating())
+                .categories(categories)
+                .streamings(streamings)
+                .build();
+    }
 }
--- src/main/java/com/dickel/movieflix/request/MovieRequest.java
+++ src/main/java/com/dickel/movieflix/request/MovieRequest.java
@@ -1,4 +1,9 @@
 package com.dickel.movieflix.request;
 
-public record MovieRequest() {
+import com.dickel.movieflix.entity.Category;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record MovieRequest(String title, String description, LocalDate releaseDate, double rating, List<Long> categories, List<Long> streamings) {
 }
