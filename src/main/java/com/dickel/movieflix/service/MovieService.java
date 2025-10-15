9fed24a7b6fae80d192e68cfdebeb0c8c0256217
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-14 23:03:04 -0300

    feat: Adiciona MovieService com regras de negócio.

--- src/main/java/com/dickel/movieflix/service/MovieService.java
+++ src/main/java/com/dickel/movieflix/service/MovieService.java
@@ -1,4 +1,23 @@
 package com.dickel.movieflix.service;
 
+import com.dickel.movieflix.entity.Movie;
+import com.dickel.movieflix.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public Movie save(Movie movie){
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> findAll(){
+        return movieRepository.findAll();
+    }
 }
