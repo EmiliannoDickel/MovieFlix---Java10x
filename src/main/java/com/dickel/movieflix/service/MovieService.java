20af8a7a869bc47909fe80058c00282ba2ee940f
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-21 20:28:12 -0300

    refactor: Adicionado tratamento no MovieService para retornar categories e streamings presentes no filme adicionado

--- src/main/java/com/dickel/movieflix/service/MovieService.java
+++ src/main/java/com/dickel/movieflix/service/MovieService.java
@@ -1,10 +1,13 @@
 package com.dickel.movieflix.service;
 
+import com.dickel.movieflix.entity.Category;
 import com.dickel.movieflix.entity.Movie;
+import com.dickel.movieflix.entity.Streaming;
 import com.dickel.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,12 +15,32 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    private List<Category> findCategories (List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> {
+            categoryService.findById(category.getId()).ifPresent(categoriesFound::add);
+        });
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings (List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> {
+            streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add);
+        });
+        return streamingsFound;
     }
 }
