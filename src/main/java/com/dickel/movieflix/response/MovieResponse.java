d3687532359d6d2511e1cdd65308ac051c7002f5
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-21 20:41:23 -0300

    refactor: ajustado para retornar a data em formato padrao no record movieRequest e movieResponse

--- src/main/java/com/dickel/movieflix/response/MovieResponse.java
+++ src/main/java/com/dickel/movieflix/response/MovieResponse.java
@@ -1,10 +1,17 @@
 package com.dickel.movieflix.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieResponse (Long id, String title, String description, LocalDate releaseDate, Double rating, List<CategoryResponse> categories, List<StreamingResponse> streamings) {
+public record MovieResponse (Long id,
+                             String title,
+                             String description,
+                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") LocalDate releaseDate,
+                             Double rating,
+                             List<CategoryResponse> categories,
+                             List<StreamingResponse> streamings) {
 }
