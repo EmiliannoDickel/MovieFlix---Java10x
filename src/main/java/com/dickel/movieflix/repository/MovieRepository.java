a46c4c2417017cecbeb2766a464b26d440c3ac7d
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-21 23:14:04 -0300

    feat: Adicionado consulta JPA no MovieRepostitory para pegar os filmes pela categoriaID.

--- src/main/java/com/dickel/movieflix/repository/MovieRepository.java
+++ src/main/java/com/dickel/movieflix/repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package com.dickel.movieflix.repository;
 
+import com.dickel.movieflix.entity.Category;
 import com.dickel.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
 }
