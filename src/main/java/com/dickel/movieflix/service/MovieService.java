2ecc29ce7fe9d86206ee70562e772fb2650f19d2
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-21 23:15:12 -0300

    feat: Finalizado implementacao dos Service, adicionado delete e update no MovieService.

--- src/main/java/com/dickel/movieflix/service/MovieService.java
+++ src/main/java/com/dickel/movieflix/service/MovieService.java
@@ -7,6 +7,7 @@ import com.dickel.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +32,41 @@ public class MovieService {
 
     public Optional<Movie> findById(Long id){
         return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> update (Long id, Movie updateMovie){
+        Optional<Movie> optMovie = movieRepository.findById(id);
+        if (optMovie.isPresent()) {
+            List<Category> categories = this.findCategories(updateMovie.getCategories());
+            List<Streaming> streamings = this.findStreamings(updateMovie.getStreamings());
+
+            Movie movie = optMovie.get();
+            movie.setTitle(updateMovie.getTitle());
+            movie.setDescription(updateMovie.getDescription());
+            movie.setReleaseDate(updateMovie.getReleaseDate());
+            movie.setRating(updateMovie.getRating());
+
+            movie.getCategories().clear();
+            movie.getCategories().addAll(categories);
+
+            movie.getStreamings().clear();
+            movie.getStreamings().addAll(streamings);
+
+            movieRepository.save(movie);
+            return Optional.of(movie);
+        }
+        return Optional.empty();
+    }
+
+    public List<Movie> findByCategory (Long categoryId){
+        return movieRepository.findMovieByCategories(List.of(Category.
+                builder().
+                id(categoryId).
+                build()));
+    }
+
+    public void delete (Long id){
+        movieRepository.deleteById(id);
     }
 
     private List<Category> findCategories (List<Category> categories){
