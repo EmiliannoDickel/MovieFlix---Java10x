07a3afca01ffc48a2c1e4f15c93703a3b80fc167
EmiliannoDickel <emilianno.dickel@universo.univates.br>
2025-10-22 21:02:13 -0300

    feat: Adicionado Entity User.

---
+++ src/main/java/com/dickel/movieflix/entity/User.java
@@ -0,0 +1,28 @@
+package com.dickel.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+
+    private String name;
+    @Column(nullable = false)
+
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
